9a5323da75a9ef67457b8f4ecd667041cd39fd91
SeanCsc <zhipengy@umich.edu>
2018-01-05 20:07:20 -0500

    leaf_classification
    combine the image and features

---
+++ leaf_species_classification.py
@@ -0,0 +1,182 @@
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedShuffleSplit
+
+# If you want to use Theano, all you need to change
+# is the dim ordering whenever you are dealing with
+# the image array. Instead of
+# (samples, rows, cols, channels) it should be
+# (samples, channels, rows, cols)
+
+# Keras stuff
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.image import img_to_array, load_img
+
+# A large amount of the data loading code is based on najeebkhan's kernel
+# Check it out at https://www.kaggle.com/najeebkhan/leaf-classification/neural-network-through-keras
+root = '../input'
+np.random.seed(2016)
+split_random_state = 7
+split = .9
+
+
+def load_numeric_training(standardize=True):
+    """
+    Loads the pre-extracted features for the training data
+    and returns a tuple of the image ids, the data, and the labels
+    """
+    # Read data from the CSV file
+    data = pd.read_csv(os.path.join(root, 'train.csv'))
+    ID = data.pop('id')
+
+    # Since the labels are textual, so we encode them categorically
+    y = data.pop('species')
+    y = LabelEncoder().fit(y).transform(y)
+    # standardize the data by setting the mean to 0 and std to 1
+    X = StandardScaler().fit(data).transform(data) if standardize else data.values
+
+    return ID, X, y
+
+
+def load_numeric_test(standardize=True):
+    """
+    Loads the pre-extracted features for the test data
+    and returns a tuple of the image ids, the data
+    """
+    test = pd.read_csv(os.path.join(root, 'test.csv'))
+    ID = test.pop('id')
+    # standardize the data by setting the mean to 0 and std to 1
+    test = StandardScaler().fit(test).transform(test) if standardize else test.values
+    return ID, test
+
+
+def resize_img(img, max_dim=96):
+    """
+    Resize the image to so the maximum side is of size max_dim
+    Returns a new image of the right size
+    """
+    # Get the axis with the larger dimension
+    max_ax = max((0, 1), key=lambda i: img.size[i])
+    # Scale both axes so the image's largest dimension is max_dim
+    scale = max_dim / float(img.size[max_ax])
+    return img.resize((int(img.size[0] * scale), int(img.size[1] * scale)))
+
+
+def load_image_data(ids, max_dim=96, center=True):
+    """
+    Takes as input an array of image ids and loads the images as numpy
+    arrays with the images resized so the longest side is max-dim length.
+    If center is True, then will place the image in the center of
+    the output array, otherwise it will be placed at the top-left corner.
+    """
+    # Initialize the output array
+    # NOTE: Theano users comment line below and
+    X = np.empty((len(ids), max_dim, max_dim, 1))
+    # X = np.empty((len(ids), 1, max_dim, max_dim)) # uncomment this
+    for i, idee in enumerate(ids):
+        # Turn the image into an array
+        x = resize_img(load_img(os.path.join(root, 'images', str(idee) + '.jpg'), grayscale=True), max_dim=max_dim)
+        x = img_to_array(x)
+        # Get the corners of the bounding box for the image
+        # NOTE: Theano users comment the two lines below and
+        length = x.shape[0]
+        width = x.shape[1]
+        # length = x.shape[1] # uncomment this
+        # width = x.shape[2] # uncomment this
+        if center:
+            h1 = int((max_dim - length) / 2)
+            h2 = h1 + length
+            w1 = int((max_dim - width) / 2)
+            w2 = w1 + width
+        else:
+            h1, w1 = 0, 0
+            h2, w2 = (length, width)
+        # Insert into image matrix
+        # NOTE: Theano users comment line below and
+        X[i, h1:h2, w1:w2, 0:1] = x
+        # X[i, 0:1, h1:h2, w1:w2] = x  # uncomment this
+    # Scale the array values so they are between 0 and 1
+    return np.around(X / 255.0)
+
+
+def load_train_data(split=split, random_state=None):
+    """
+    Loads the pre-extracted feature and image training data and
+    splits them into training and cross-validation.
+    Returns one tuple for the training data and one for the validation
+    data. Each tuple is in the order pre-extracted features, images,
+    and labels.
+    """
+    # Load the pre-extracted features
+    ID, X_num_tr, y = load_numeric_training()
+    # Load the image data
+    X_img_tr = load_image_data(ID)
+    # Split them into validation and cross-validation
+    sss = StratifiedShuffleSplit(n_splits=1, train_size=split, random_state=random_state)
+    train_ind, test_ind = next(sss.split(X_num_tr, y))
+    X_num_val, X_img_val, y_val = X_num_tr[test_ind], X_img_tr[test_ind], y[test_ind]
+    X_num_tr, X_img_tr, y_tr = X_num_tr[train_ind], X_img_tr[train_ind], y[train_ind]
+    return (X_num_tr, X_img_tr, y_tr), (X_num_val, X_img_val, y_val)
+
+
+def load_test_data():
+    """
+    Loads the pre-extracted feature and image test data.
+    Returns a tuple in the order ids, pre-extracted features,
+    and images.
+    """
+    # Load the pre-extracted features
+    ID, X_num_te = load_numeric_test()
+    # Load the image data
+    X_img_te = load_image_data(ID)
+    return ID, X_num_te, X_img_te
+
+print('Loading the training data...')
+(X_num_tr, X_img_tr, y_tr), (X_num_val, X_img_val, y_val) = load_train_data(random_state=split_random_state)
+y_tr_cat = to_categorical(y_tr)
+y_val_cat = to_categorical(y_val)
+print('Training data loaded!')
+[X_num_tr,X_img_tr]
+
+from keras.models import Model 
+from keras.layers import Dense,Dropout,Activation,Conv2D,MaxPooling2D,Flatten,Input,merge
+
+def combined_model():
+    image = Input(shape = (96,96,1),name = 'image')
+    x = Conv2D(8,5,5,input_shape = (96,96,1),border_mode = 'same')(image)
+    x = (Activation('relu'))(x)
+    x = (MaxPooling2D(pool_size = (2,2), strides = (2,2)))(x)
+    x = (Conv2D(32, 5, 5, border_mode='same'))(x)
+    x = (Activation('relu'))(x)
+    x = (MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))(x)
+      # Flatten our array
+    x = Flatten()(x)
+    feature = Input(shape = (192,),name = 'feature')
+    concate = merge([x,feature],mode = 'concat')
+    x = Dense(100,activation = 'relu')(concate)
+    x = Dropout(.5)(x)
+    out = Dense(99,activation = 'softmax')(x)
+    model = Model(input = [image,feature],output =out)
+    model.compile(loss = 'categorical_crossentropy', optimizer = 'rmsprop', metrics = ['accuracy'])
+    return model
+print('Creating the model...')
+model = combined_model()
+print('Model created!')
+nb_epoch = 15
+model.fit([X_img_tr, X_num_tr], y_tr_cat,epochs = nb_epoch,validation_data =([X_img_val, X_num_val],y_val_cat),verbose = 0,batch_size = 15 )
+
+LABELS = sorted(pd.read_csv('../input/train.csv').species.unique())
+index, X_fea, X_img_te = load_test_data()
+
+y_pred = model.predict([X_img_te,X_fea])
+
+yp = pd.DataFrame(y_pred,index = index, columns = LABELS)
+
+print('Creating and writing submission...')
+fp = open('submit.csv', 'w')
+fp.write(yp.to_csv())
+print('Finished writing submission')
